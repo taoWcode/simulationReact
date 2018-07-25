826d5d66dea1319f832ce81f6bf8381bec178a67
weitao <itophp@itophp.com>
2018-07-25 11:08:49 +0800

    创建本地数据data，设置一个setData方法赋予其操作数据的能力

--- js/react.js
+++ js/react.js
@@ -7,4 +7,12 @@
 
 /*target:实现一个组件，能够监听数据的变化，动态的维护视图，处理UI与用户的交互*/
 
-var component = {};
+var component = {
+	data:{
+		name:'Jack'
+	},
+	setData:function(key, val){
+		var _t = this;
+		_t.date[key] = val;
+	}
+};
