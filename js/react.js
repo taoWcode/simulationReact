d5998bb621f8e9be306cc598943ddc77d231fb97
weitao <itophp@itophp.com>
2018-07-25 11:14:05 +0800

    给一个render方法并创建一个id为root的根节点

--- js/react.js
+++ js/react.js
@@ -8,11 +8,17 @@
 /*target:实现一个组件，能够监听数据的变化，动态的维护视图，处理UI与用户的交互*/
 
 var component = {
+	root:document.getElementById('root'),
 	data:{
 		name:'Jack'
 	},
 	setData:function(key, val){
 		var _t = this;
 		_t.date[key] = val;
+	},
+	render:function(){
+		var _t = this;
+		var template = `<div>Hello,world!</div>`;
+		_t.root.innerHTML = template;
 	}
 };
